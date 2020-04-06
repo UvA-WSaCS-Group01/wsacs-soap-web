9a5182151917fc0aa3b898c003bb3e46c818c3e9
Stijn Meijerink <stijn.meijerink@gmail.com>
2020-04-06 10:50:28 +0200

    Removed hello world stuff

--- src/main/java/com/wsacs/wsacs/soap/web/calculator/CalculatorService.java
+++ src/main/java/com/wsacs/wsacs/soap/web/calculator/CalculatorService.java
@@ -1,30 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.wsacs.wsacs.soap.web.calculator;
 
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 
-/**
- *
- * @author mail
- */
 @WebService(serviceName = "CalculatorService")
 public class CalculatorService {
 
-    /**
-     * This is a sample web service operation
-     */
-    @WebMethod(operationName = "hello")
-    public String hello(@WebParam(name = "name") String txt) {
-        return "Hello " + txt + " !";
-    }
-    
-    
     @WebMethod(operationName = "add")
     public double add(@WebParam(name = "x") double x, @WebParam(name = "y") double y) {
         return x+y;
